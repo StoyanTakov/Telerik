8a511fa7335cb4b9e82c42572e7a0281335a3adb
p3rf0rat0ra <p3rf0rat0ra@gmail.com>
2016-05-01 23:31:36 +0300

    updating

---
+++ C#/C#Part1/Exams/TelerikExam2015Morning/Cube/Cube.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Cube
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int fieldSize = n * 2 - 1;
+        int mid = n - 1;
+        char emtpy = ' ';
+        char top = '/';
+        char side = 'X';
+        char edge = ':';
+        for (int row = 0; row < fieldSize; row++)
+        {
+            for (int col = 0; col < fieldSize; col++)
+            {
+                if (col==0 && row>=mid ||
+                    col==mid && row>=mid ||
+                    col==fieldSize-1 && row<=mid ||
+                    col >=mid && row==0 ||
+                    col <=mid && row ==mid ||
+                    col <= mid && row == fieldSize-1||
+                    col + row == mid ||
+                    col + row == fieldSize-1 +mid ||
+                    col + row == fieldSize - 1 && row<=mid)
+                {
+                    Console.Write(edge);
+                }
+                else if(col >mid &&
+                    col + row <= fieldSize - 1 + mid &&
+                    col + row >= fieldSize - 1)
+                {
+                    Console.Write(side);
+                }
+                else if(row < mid && col + row >= mid)
+                {
+                    Console.Write(top);
+                }
+                else
+                {
+                    Console.Write(emtpy);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
