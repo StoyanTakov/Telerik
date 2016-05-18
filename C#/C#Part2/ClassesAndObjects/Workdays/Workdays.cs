419726189a15af7fe5c5db5feb6667e868856616
p3rf0rat0ra <p3rf0rat0ra@gmail.com>
2016-05-18 16:44:49 +0300

    upload ClassesAndObjects homework

---
+++ C#/C#Part2/ClassesAndObjects/Workdays/Workdays.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
+
+namespace Workdays
+{
+
+    class Workdays
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Console.WriteLine("Enter a date (yyyy,MM,dd): ");
+            DateTime targetDate = DateTime.Parse(Console.ReadLine()); //yyyy, MM, dd
+            Console.WriteLine("There are {0} working days until {1:D}.", CalcWorkdays.CountWorkdays(targetDate), targetDate);
+        }
+
+    }
+
+    static class CalcWorkdays
+    {
+        public static int CountWorkdays(DateTime target)
+        {
+            DateTime today = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            int count = 0;
+
+            while (today != target)
+            {
+                today = today.AddDays(1); //"moving" today forward and counting the days that are not Saturday, Sunday or predefined holiday
+                if (today.DayOfWeek != DayOfWeek.Saturday &&
+                    today.DayOfWeek != DayOfWeek.Sunday &&
+                    !today.IsHoliday())
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static bool IsHoliday(this DateTime date)
+        {
+            DateTime[] holidays =   //some holidays are not on a fixed date; add dates according to needs
+            {
+          new DateTime(date.Year,01,01),
+          new DateTime(date.Year,03,03),
+          new DateTime(date.Year,05,06),
+          new DateTime(date.Year,05,24),
+          new DateTime(date.Year,09,06),
+          new DateTime(date.Year,09,22),
+          new DateTime(date.Year,12,25),
+          new DateTime(date.Year,12,26)
+        };
+
+            return holidays.Contains(date.Date);
+        }
+    }
+}
