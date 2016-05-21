698ba70f768369fbc128da3c2db21db8c198a090
p3rf0rat0ra <p3rf0rat0ra@gmail.com>
2016-05-21 13:42:57 +0300

    uploading StringsAndTextProcessing homework

---
+++ C#/C#Part2/StringsAndTextProcessing/SubstringInText/SubstringInText.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class SubstringInText
+{
+    static int counter;
+    static int CheckText (string text, string subText)
+    {
+        counter = 0;
+        int nextIndexOfSubText = text.IndexOf(subText);
+        while (text.IndexOf(subText)!=-1)
+        {
+            counter++;
+            nextIndexOfSubText = text.IndexOf(subText, nextIndexOfSubText + 1);
+        }
+        return counter;
+    }
+    static void Main()
+    {
+        string subText = Console.ReadLine();
+        string text = Console.ReadLine();
+        counter = CheckText(text, subText);
+        Console.WriteLine(counter);
+    }
+}
+
